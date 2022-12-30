d345484687f73d3824112c71e7a67e4a504af1a6
oratiss <massoud.asgariyan@gmail.com>
2022-12-30 18:23:59 +0330

    Adding Domain Test Project and Domain Project - starting TDD

---
+++ src/DomainTest/Customers/CustomerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainTest.Customers.TestBuilders;
+
+namespace DomainTest.Customers
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public async Task FirstName_Should_Not_Be_NullORWhiteSpace()
+        {
+            //arrange
+            var customer = new CustomerDomainTestBuilder();
+            //.With(x => x.FirstName, string.Empty)
+            //.Build();
+        }
+    }
+}
